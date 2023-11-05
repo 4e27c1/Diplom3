eec9a56b641bfb5150df58b75424c3d5d2aeef8c
A Akchurina <4e27c1@gmail.com>
2023-11-06 00:05:57 +0300

    review fixes part2

--- src/main/java/praktikum/pages/Environment.java
+++ src/main/java/praktikum/pages/Environment.java
@@ -10,8 +10,7 @@ public class Environment {
     public static final String USER_CREATE = "api/auth/register";
     public static final String USER_DELETE = "/api/auth/user";
     public static final int DEFAULT_TIMEOUT = 10;
-
-    public static final String CHROME_DRIVER = System.getProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-    public static final String YANDEX_DRIVER = System.getProperty("webdriver.chrome.driver", "src/main/resources/chromedriver114.exe");
-    public static final String YANDEX_BINARY = System.getProperty("webdriver.yandex.binary", "C:\\Program Files\\Yandex\\YandexBrowser\\Application\\browser.exe");
+    public static final String CHROME_DRIVER = System.getProperty("webdriver.chrome.driver");
+    public static final String YANDEX_DRIVER = System.getProperty("webdriver.yandex.driver");
+    public static final String YANDEX_BINARY = System.getProperty("webdriver.yandex.binary");
 }
